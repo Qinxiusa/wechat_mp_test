ac0c845a6b994b606518e65e8217b0d782abfd7a
cs.chen <cs.chen@tpv-tech.com>
2022-08-05 13:35:19 +0800

    UPDATE TABLE CONTENT

--- src/main/resources/db/migration/V20220805_01__add_table_messages.sql
+++ src/main/resources/db/migration/V20220805_01__add_table_messages.sql
@@ -1,8 +1,14 @@
-CREATE TABLE IF NOT EXISTS 'Messages'(
-	'id' int(11) NOT NULL AUTO_INCREMENT,
-	'UserName' varchar(30) default NULL,
-	'type' varchar(20) default NULL,
-	'content' TEXT default NULL,
-	'create'  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY('id')
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
+CREATE TABLE  IF NOT EXISTS `Messages` (
+
+`id` int(11) NOT NULL default '0',
+
+`user_name` varchar(30) default NULL,
+
+`msg_type` varchar(20) default NULL,
+
+`content` text default NULL,
+
+`create_time` varchar(20) default NULL,
+
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
